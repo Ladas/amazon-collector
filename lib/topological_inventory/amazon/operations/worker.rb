f30f8941261d577f17da197dda21c9784444e585
Patrik Komanek <pkomanek@redhat.com>
2020-11-03 13:14:46 +0100

    Modify operations worker to use new messaging_client.

--- lib/topological_inventory/amazon/operations/worker.rb
+++ lib/topological_inventory/amazon/operations/worker.rb
@@ -1,4 +1,5 @@
 require "topological_inventory/amazon/logging"
+require "topological_inventory/amazon/messaging_client"
 require "topological_inventory/amazon/operations/processor"
 require "topological_inventory/amazon/operations/source"
 require "topological_inventory/providers/common/operations/health_check"
@@ -9,26 +10,27 @@ module TopologicalInventory
       class Worker
         include Logging
 
-        def initialize(messaging_client_opts = {})
-          self.messaging_client_opts = default_messaging_opts.merge(messaging_client_opts)
-        end
-
         def run
-          # Open a connection to the messaging service
-          require "manageiq-messaging"
-          client = ManageIQ::Messaging::Client.open(messaging_client_opts)
-
           logger.info("Topological Inventory Amazon Operations worker started...")
+
           client.subscribe_topic(queue_opts) do |message|
             process_message(message)
           end
+        rescue => err
+          logger.error("#{err.cause}\n#{err.backtrace.join("\n")}")
         ensure
           client&.close
         end
 
         private
 
-        attr_accessor :messaging_client_opts
+        def client
+          @client ||= TopologicalInventory::Amazon::MessagingClient.default.worker_listener
+        end
+
+        def queue_opts
+          TopologicalInventory::Amazon::MessagingClient.default.worker_listener_queue_opts
+        end
 
         def process_message(message)
           Processor.process!(message)
@@ -38,27 +40,6 @@ module TopologicalInventory
         ensure
           message.ack
           TopologicalInventory::Providers::Common::Operations::HealthCheck.touch_file
-        end
-
-        def queue_name
-          "platform.topological-inventory.operations-amazon"
-        end
-
-        def queue_opts
-          {
-            :auto_ack    => false,
-            :max_bytes   => 50_000,
-            :service     => queue_name,
-            :persist_ref => "topological-inventory-operations-amazon"
-          }
-        end
-
-        def default_messaging_opts
-          {
-            :protocol   => :Kafka,
-            :client_ref => "topological-inventory-operations-amazon",
-            :group_ref  => "topological-inventory-operations-amazon"
-          }
         end
       end
     end
