78744e4d8a0afe645c042dc4c0dadea60bb87b0e
Martin Slemr <mslemr@redhat.com>
2020-12-14 14:41:05 +0100

    Common metrics init

--- lib/topological_inventory/amazon/collector/metrics.rb
+++ lib/topological_inventory/amazon/collector/metrics.rb
@@ -1,4 +1,3 @@
-require 'benchmark'
 require 'topological_inventory/providers/common/metrics'
 
 module TopologicalInventory
@@ -9,27 +8,6 @@ module TopologicalInventory
 
         def initialize(port = 9394)
           super(port)
-
-          init_counters
-        end
-
-        def record_refresh_timing(labels = {}, &block)
-          record_time(@refresh_timer, labels, &block)
-        end
-
-        private
-
-        def configure_metrics
-          super
-
-          @refresh_timer = PrometheusExporter::Metric::Histogram.new('refresh_time', 'Duration of full refresh')
-          @server.collector.register_metric(@refresh_timer)
-        end
-
-        def init_counters
-          ERROR_TYPES.each do |err_type|
-            @error_counter&.observe(0, :type => err_type)
-          end
         end
 
         def default_prefix
